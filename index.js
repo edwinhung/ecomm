ed5eb190c08300c006f3198aaa84eb6aec3aec27
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-07-11 18:01:21 -0700

    send html back

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi there");
+  res.send(`
+    <div>
+        <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <input name="passwordConfirmation" placeholder="password confirmation" />
+            <button>Sign up</button>
+        </form>
+    </div>
+    `);
 });
 
-app.listen(3000, () => console.log("Listening..."));
+app.post("/", (req, res) => {
+  res.send("Account Created!");
+});
+
+app.listen(3001, () => console.log("Listening..."));
