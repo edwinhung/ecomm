1f5203f33b0506519979ca8acd60347ee60c9519
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-07-13 17:36:28 -0700

    add getOne

--- repositories/users.js
+++ repositories/users.js
@@ -41,13 +41,18 @@ class UsersRepository {
   randomId() {
     return crypto.randomBytes(4).toString("hex");
   }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((record) => record.Id === id);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
-  await repo.create({ email: "test@test.com", password: "password" });
-  const users = await repo.getAll();
-  console.log(users);
+
+  const user = await repo.getOne("8b53124d");
+  console.log(user);
 };
 
 test();
