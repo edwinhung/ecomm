8f4991f425c1d2152c766691caf0a9cc2061ca48
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-07-17 10:47:11 -0700

    add products page

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,9 +1,12 @@
 const express = require("express");
-
+const productsRepo = require("../../repositories/products");
 const router = express.Router();
+const productsNewTempalte = require("../../views/admin/products/new");
 
 router.get("/admin/products", (req, res) => {});
 
-router.get("/admin/products/new", (req, res) => {});
+router.get("/admin/products/new", (req, res) => {
+  res.send(productsNewTempalte({}));
+});
 
 module.exports = router;
